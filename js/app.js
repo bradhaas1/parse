3c277ba1ed9e36572c8ce591e753c54c96e41d15
Brad Haas <brad.haas@nbcuni.com>
2014-08-08 16:16:15 -0700

    started send messages

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
   var parseID = "UR5O55W9prxCEH2lWy7ZvUaj7REtBgE0bbMNRG7y";
   var parseRestKey = "gFx9Kmn29lVc8GrqOcAjrOZ5s7GplpolUHEyRuJN";
 
-  getMessages();
+//  getMessages();
 
+  $("#send").click(function(){
+    var username = $("input[name=username]").attr("value");
+    var msg = $("input[name=message]").attr("value");
 
-
+  })
 });
